513acd99380abcbcf6161be50df75c9bb60a8be4
Christopher Buck <cbuck@JLAB.ORG>
2016-01-27 16:14:24 -0500

    Some basic structure

---
+++ src/C100.java
@@ -0,0 +1,29 @@
+
+public class C100 extends CryomoduleType
+{
+	public C100()
+	{
+		this.name = "C100";
+		
+	}
+	
+	@Override
+	public void importRawData()
+	{
+		
+	}
+
+	@Override
+	public void exportTravelerData()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void prepareChartData()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/CryomoduleType.java
@@ -0,0 +1,12 @@
+
+public abstract class CryomoduleType
+{
+	public String name;
+	
+	public abstract void importRawData();
+	
+	public abstract void exportTravelerData();
+	
+	public abstract void prepareChartData();
+	
+}
